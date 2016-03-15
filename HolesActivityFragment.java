db10acae2b25ec0fbd9a65e4aa45619f1a61c838
David Terpin <davidterpin@gmail.com>
2016-03-15 12:56:15 +1100

    Initial commit

---
+++ HolesActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.stickman.golfholesv1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class HolesActivityFragment extends Fragment {
+
+    public HolesActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_holes, container, false);
+    }
+}
